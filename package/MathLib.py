2dcaa70485c663f07495607407c3c8dedd825c05
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-06-02 21:52:32 -0300

    exponential

--- package/MathLib.py
+++ package/MathLib.py
@@ -29,3 +29,36 @@ def divide(arg=[0]):
     for i in range(1, len(arg)):
         res /= arg[i]
     return res
+
+
+def xpnt(arg=[0]):
+    res = arg[0]
+    if len(arg) == 1:
+        return res**2
+    for i in range(1, len(arg)):
+        res = res**arg[i]
+    return res
+
+
+def nRoot(arg0=1, arg1=2):
+    res = 0.0
+    tmp = 0.0
+    for i in range(arg0):
+        if xpnt([i, arg1]) == arg0:
+            res = i
+            break
+        elif xpnt([i, arg1]) >= arg0:
+            tmp = i-1
+            break
+    if tmp > 0:
+        cond = True
+        modifier = 0.1
+        while cond:
+            if xpnt([tmp+0.1, arg1]) < arg0:
+                tmp += modifier
+            else:
+                res = tmp
+                cond = False
+
+    res = float(format(res, ".1f"))
+    return res
