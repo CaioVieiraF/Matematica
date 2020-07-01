a46c6fdbe625bc675d8d3cf3da5d0204d8dc537d
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-07-01 01:44:09 -0300

    docs conf file update

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,3 +55,5 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+pygments_style = 'sphinx'
